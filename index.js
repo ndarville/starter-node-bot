b8276c1673d9df75c0edcc1810c0569deaa3b1bf
Niclas Darville <contact@ndarville.com>
2016-03-29 16:44:19 +0200

    Fix semicolons and one wrong indentation

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 var Botkit = require("botkit");
 
 // Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
+var slackToken = process.env.SLACK_TOKEN;
 if (!slackToken) {
-    console.error("SLACK_TOKEN is required!")
-    process.exit(1)
+    console.error("SLACK_TOKEN is required!");
+    process.exit(1);
 }
 
-var controller = Botkit.slackbot()
+var controller = Botkit.slackbot();
 var bot = controller.spawn({
     token: slackToken
-})
+});
 
 bot.startRTM(function (err, bot, payload) {
     if (err) {
-        throw new Error("Could not connect to Slack")
+        throw new Error("Could not connect to Slack");
     }
-})
+});
 
 controller.on("bot_channel_join", function (bot, message) {
-    bot.reply(message, "I'm here!")
-})
+    bot.reply(message, "I'm here!");
+});
 
 // Mention listener
 controller.hears(["hello", "hi", "sup", "howdy", "yo"], ["direct_mention"], function (bot, message) {
-    bot.reply(message, "sup")
-})
+    bot.reply(message, "sup");
+});
 
 // DM listener
 controller.hears(["hello", "hi", "sup", "howdy", "yo"], ["direct_message"], function (bot, message) {
-    bot.reply(message, "Wuddup.")
-    bot.reply(message, "It\'s nice to talk to you directly.")
-})
+    bot.reply(message, "Wuddup.");
+    bot.reply(message, "It\'s nice to talk to you directly.");
+});
 
 controller.hears(".*", ["mention"], function (bot, message) {
-    bot.reply(message, "You really do care about me. :heart:")
-})
+    bot.reply(message, "You really do care about me. :heart:");
+});
 
 controller.hears("help", ["direct_message", "direct_mention"], function (bot, message) {
     var help = "I will respond to the following messages: \n" +
         "`bot hi` for a simple message.\n" +
         "`bot attachment` to see a Slack attachment message.\n" +
         "`@<your bot\'s name>` to demonstrate detecting a mention.\n" +
-        "`bot help` to see this again."
-    bot.reply(message, help)
-})
+        "`bot help` to see this again.";
+    bot.reply(message, help);
+});
 
 controller.hears(["attachment"], ["direct_message", "direct_mention"], function (bot, message) {
-    var text = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
+    var text = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.";
     var attachments = [{
         fallback: text,
         pretext: "We bring bots to life. :sunglasses: :thumbsup:",
@@ -56,14 +56,14 @@ controller.hears(["attachment"], ["direct_message", "direct_mention"], function 
         title_link: "https://beepboophq.com/",
         text: text,
         color: "#7CD197"
-    }]
+    }];
 
     bot.reply(message, {
         attachments: attachments
     }, function (err, resp) {
-        console.log(err, resp)
-    })
-})
+        console.log(err, resp);
+    });
+});
 
 // Custom scripts
 // ==============
@@ -73,8 +73,8 @@ controller.hears(["attachment"], ["direct_message", "direct_mention"], function 
 // Expect a GOOGLE_TOKEN environment variable
 var googleToken = process.env.GOOGLE_TOKEN;
 if (!googleToken) {
-    console.error("GOOGLE_TOKEN is required!")
-    process.exit(1)
+    console.error("GOOGLE_TOKEN is required!");
+    process.exit(1);
 }
 
 var googleTranslate = require("google-translate")(googleToken);
@@ -93,5 +93,5 @@ controller.hears(["^translate (.*)$"], ["direct_message", "direct_mention"], fun
 
 // This goes by the end of the file; it works as an "else" function for listener events.
 controller.hears(".*", ["direct_message", "direct_mention"], function (bot, message) {
-  bot.reply(message, "Sorry <@" + message.user + ">, I don\'t understand. \n")
-})
+    bot.reply(message, "Sorry <@" + message.user + ">, I don\'t understand. \n");
+});
