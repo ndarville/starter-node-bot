090ca7475f0b5b3dd4cee0f5cc9f6d8ec69380a7
Niclas Darville <contact@ndarville.com>
2016-03-31 14:00:58 +0200

    Remove attachment sample code

--- index.js
+++ index.js
@@ -41,28 +41,8 @@ controller.hears("help", ["direct_message", "direct_mention"], function (bot, me
     var help = "I will respond to the following messages: \n" +
         "`bot hi` for a simple message.\n" +
         "`bot translate [words]` to translate anything into English.\n" +
-        "`bot attachment` to see a Slack attachment message.\n" +
         "`bot help` to see this again.";
     bot.reply(message, help);
-});
-
-controller.hears(["attachment"], ["direct_message", "direct_mention"], function (bot, message) {
-    var text = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.";
-    var attachments = [{
-        fallback: text,
-        pretext: "We bring bots to life. :sunglasses: :thumbsup:",
-        title: "Host, deploy and share your bot in seconds.",
-        image_url: "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
-        title_link: "https://beepboophq.com/",
-        text: text,
-        color: "#7CD197"
-    }];
-
-    bot.reply(message, {
-        attachments: attachments
-    }, function (err, resp) {
-        console.log(err, resp);
-    });
 });
 
 // Custom scripts
